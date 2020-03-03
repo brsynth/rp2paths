2f992d22f3d6c983ceeb51cec0a3cdc7a2391a45
joan <joan.herisson@univ-evry.fr>
2020-03-03 14:45:47 +0100

    unuseful

--- test/run-in-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-command=`cat run.sh`
-
-docker run --rm \
-  -v $PWD/..:/home \
-  -v $PWD:/home/test \
-  -w /home/test \
-brsynth/rp2paths bash -c "$command"
--- test/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 ../RP2paths.py all rp2_pathways.csv --outdir results --timeout 5
