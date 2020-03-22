9af7f7e05de1b9604ab232ed8a3fa243dcec7128
joan <joan.herisson@univ-evry.fr>
2020-03-22 02:19:55 +0100

    update

--- test/test-in-rest.sh
+++ test/test-in-rest.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 
-cd ../rest
-./REST-server.sh start redis
-cd -
-
-sleep 5
+# cd ../rest
+# ./REST-server.sh start redis
+# cd -
+#
+# sleep 5
 
 docker run --rm \
-  --link rp2paths-rest:rp2paths \
   -v $PWD:/home \
   -w /home \
   --net rp2paths \
 python:3 ./files/rest-query.sh
 
 
-cd ../rest
-./REST-server.sh stop
-cd -
+# cd ../rest
+# ./REST-server.sh stop
+# cd -
