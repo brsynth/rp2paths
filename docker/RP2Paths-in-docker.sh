8af31a218eb21271ca8dd39b6dac27ed86a5c34d
joan <joan.herisson@univ-evry.fr>
2020-03-26 15:41:42 +0100

    add code in Dockerfile

--- docker/RP2Paths-in-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose run --rm -w /home/src rp2paths python RP2paths.py $@
--- test/test-in-docker.sh
+++ test/test-in-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ../docker
-docker-compose run --rm rp2paths \
-  sh -c "cd test ; ./test-standalone.sh out/test-in-docker"
+docker-compose run --rm -v $PWD/../test:/home/test -w /home/test --entrypoint="" rp2paths \
+  sh -c "./test-standalone.sh out/test-in-docker"
 cd -
