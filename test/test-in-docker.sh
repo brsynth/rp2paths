e96f146e9c7cfa65312b3f360ed6eb8b6aaf0e60
joan <joan.herisson@univ-evry.fr>
2020-03-31 14:52:19 +0200

    outdir param

--- test/test-in-docker.sh
+++ test/test-in-docker.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+outdir=$1
+
+if [[ "$outdir" == "" ]]; then
+  outdir="out/test-in-docker"
+fi
+
 cd ../docker
 docker-compose run --rm -v $PWD/../test:/home/test -w /home/test --entrypoint="" rp2paths \
-  sh -c "./test-standalone.sh out/test-in-docker"
+  sh -c "./test-standalone.sh $outdir"
 cd -
