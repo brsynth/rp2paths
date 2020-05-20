d105c9128954d581ad02f64eb4b4b4ca42e3760c
joan <joan.herisson@univ-evry.fr>
2020-05-20 10:09:49 +0200

    add default values

--- test/run-in-docker.sh
+++ test/run-in-docker.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-infile=$1
-outdir=$2
-
 cd ../docker
 docker-compose run --rm -v $PWD/../test:/home/test -w /home/test --entrypoint="" rp2paths \
-  sh -c "./run.sh $infile $outdir"
+  sh -c "./run.sh"
 cd -
--- test/run.sh
+++ test/run.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-infile=$1
-if [[ "$infile" == "" ]]; then
-  echo
-  echo "*** Usage is:"
-  echo
-  echo "     $0 <infile> [outdir]"
-  echo
-  echo
-  exit 1
-fi
-
-outdir=$2
-if [[ "$outdir" == "" ]]; then
-  outdir="out/test"
-fi
 
 
-python3 ../src/RP2paths.py all $infile --outdir $outdir --timeout 5
+python3 ../src/RP2paths.py all in/rp2_pathways.csv --outdir out --timeout 5
